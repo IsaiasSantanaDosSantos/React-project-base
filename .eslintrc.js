f8e4eebdb6dba45f9d1bb060e81e1c6360cefa91
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2023-04-05 22:37:13 -0300

    install history

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,12 @@ module.exports = {
   },
   plugins: ["react"],
   rules: {
+    "prettier/prettier": 2,
+    "react/jsx-filename-extension": 0,
+    "import/prefer-default-export": 0,
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/react-in-jsx-scope": "off",
     "no-unused-vars": [
       "error",
       {
