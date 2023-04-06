26e21bfad9ef3fca8f48bb7724c846575375f607
Isaias Santana dos Santos <65987820+IsaiasSantanaDosSantos@users.noreply.github.com>
2023-04-05 21:49:14 -0300

    remove comments

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,3 @@
-// semi = true;
-// tabWidth = 2;
-// printWidth = 100;
-// singleQuote = false;
-// trailingComma = "es5";
-// jsxBracketSameLine = true;
 module.exports = {
   semi: true,
   tabWidth: 2,
