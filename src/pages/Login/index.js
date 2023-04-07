08609397b00223fac081d42e87deb3cf81c09854
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2023-04-07 18:25:24 -0300

    installation and configuration of the Axios

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,7 +3,10 @@ import React from 'react';
 import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragrafo } from './styled';
 
+
 export default function Login() {
+
+
   return (
     <Container>
       <Title>
